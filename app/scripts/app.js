becbf17f3c000efea8a81a4b34b9d36586368fe7
Marco Viappiani <marco.viappiani@gmail.com>
2017-06-14 22:14:36 +0100

    User Story: available chat rooms

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,11 @@
             url: '/',
             controller: 'HomeCtrl as home',
             templateUrl: '/templates/home.html'
+        })
+            .state('room', {
+            url: '/room',
+            controller: 'RoomCtrl as room',
+            templateUrl: '/templates/home.html'
         });
     }
     
