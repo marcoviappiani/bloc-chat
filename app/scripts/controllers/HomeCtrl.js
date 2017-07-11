bb838c7b9305f2e868b09c7d18b97d6df5703de6
Marco Viappiani <marco.viappiani@gmail.com>
2017-07-11 22:20:23 +0100

    Can create chat room

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,21 @@
 (function(){
-    function HomeCtrl(Room) {
+    function HomeCtrl($uibModal,Room) {
         this.rooms = Room.all;
+        console.log(Room.all);
+        
+        this.openModal = function(size, template){
+            console.log('opening modal');
+            $uibModal.open({
+                animtion: true,
+                templateUrl: template || '/templates/modal.html',
+                controller: 'ModalCtrl as modal',
+                backdrop: 'static'
+            });
+        };    
     }
+    
     
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['$uibModal','Room', HomeCtrl]);
 })();
---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,37 @@
+(function(){
+    function ModalCtrl($uibModalInstance, Room) {
+        
+        var vm = this;
+        
+        //input field in the modal
+        this.roomName = '';
+        
+        this.ok = function() {
+            console.log('room name: ', vm.roomName);
+            alert("your roomName is" + vm.roomName);
+            Room.add({
+                name: this.roomName
+            });
+            
+            $uibModalInstance.close(vm.roomName);
+        };
+        
+        this.cancel = function() {
+            $uibModalInstance.dismiss('cancel');
+        };
+      
+    }
+    
+    angular
+        .module('blocChat')
+        .controller('ModalCtrl', ['$uibModalInstance','Room', ModalCtrl]);
+})();
+
+
+
+
+// - Room object outside of service.
+// decide whether to use vm or this. then use it everywhere.
+// - styling
+
+
